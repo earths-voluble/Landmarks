41901102914f310da93bfdbdbdd038f2fff7c37d
Paul Lee <earths-voluble.0g@icloud.com>
2023-07-24 18:18:37 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 이보한 on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
